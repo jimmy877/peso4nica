f6acfea62e2eaa898ea8aad27600a55aa02621d5
Anton <molot877@mail.ru>
2022-06-18 17:05:49 +0300

    polyphil added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Main</title>
 </head>
 <body>
+<script async src="https://ga.jspm.io/npm:es-module-shims@1.5.6/dist/es-module-shims.js"></script>
 <script type="importmap">
     {
         "imports":{
