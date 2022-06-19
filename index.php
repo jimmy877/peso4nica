812ab28e3955f478e67a7a7da76da9afb6e5725c
jimmy877 <molot877@mail.ru>
2022-06-19 12:46:13 +0300

    попробовал просто пути прописать и надо протестить на файрфоксе

--- index.php
+++ index.php
@@ -9,18 +9,18 @@
     <title>Main</title>
 </head>
 <body>
-<script async src="https://ga.jspm.io/npm:es-module-shims@1.5.6/dist/es-module-shims.js"></script>
+<!--<script async src="https://ga.jspm.io/npm:es-module-shims@1.5.6/dist/es-module-shims.js"></script> -->
 <script type="importmap">
     {
         "imports":{
-         "moduleOne/":"./js/"
+         "js/":"./js/"
         }
     }
 </script>
 
 <script type="module">
-    import{sayName} from "moduleOne/moduleOne.js";
-    import User from "moduleOne/moduleTwo.js";
+    import{sayName} from "/js/moduleOne.js";
+    import User from "/js/moduleTwo.js";
     sayName();
 
     let user = new User();
